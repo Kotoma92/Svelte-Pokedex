493ae1e34045aa3f783d5c12275c83ac066af9e5
Kristian Roland <kristian@rolandtech.org>
2022-01-28 13:55:22 +0100

    Main Commit

---
+++ Pokedex/src/components/nav.svelte
@@ -0,0 +1,4 @@
+<nav class="flex justify-center w-full">
+    <a class="mx-4 text-lg" href="/">Hjem</a>
+    <a class="mx-4 text-lg" href="/om">Om</a>
+</nav>
---
+++ Pokedex/src/routes/om.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>Om Svelte kit Pokedex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">Om Svelte kit Pokedex</h1>
+<p>Dette er en veldig kul Svelte kit demo som bruker Pokemon API</p>
